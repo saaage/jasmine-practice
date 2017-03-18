02edd46d50fb2265556e0ecc7caba8eb86e3c8d1
Sage911 <stefan.age@outlook.com>
2017-03-18 17:39:45 -0500

    added inputString property to index.js, fixed spacing in spec file, corrected syntax error package.json

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -6,17 +6,17 @@ describe("Extraction function", function(){
 
   // if indexController.extractTemplateValues(testString) does not throw error, then expectation is met
   it("accepts a string as an agrument", function() {
-    expect(typeof(indexController.extractTemplateValues(testString)[0])).toBe('string');
+    expect(typeof(indexController.inputString)).toBe('string');
   });
 
   // pass testString to indexController.extractTemplateValues() - should return elephant and working!
-  it("pulls values from double curly braces ", function() {
+  it("pulls values from double curly braces", function() {
     expect(indexController.extractTemplateValues(testString)).toEqual(['elephant','working!']);
   });
 
   // use Object.prototype.toString.call on our function to see what type of object it returns. Should be an
   //  array!
-  it("returns an array ", function (){
+  it("returns an array", function(){
     expect(Object.prototype.toString.call(indexController.extractTemplateValues())).toBe('[object Array]');
   });
 
